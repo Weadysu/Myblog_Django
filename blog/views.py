8c1a475ac0a63ceacbebd1c68039eccffa656987
SuHuadi <19403372@life.hkbu.edu.hk>
2020-01-25 13:25:51 +0800

    NO.9 of Django tutorial on Bilibili

--- blog/views.py
+++ blog/views.py
@@ -6,16 +6,16 @@ from .models import Blog, BlogType
 def blog_list(request):
     content = {}
     content['blogs'] = Blog.objects.all()
-    return render(request, 'blog_list.html', content)
+    return render(request, 'blog/blog_list.html', content)
 
 def blog_detail(request, blog_pk):
     content = {}
     content['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    return render(request, 'blog_detail.html', content)
+    return render(request, 'blog/blog_detail.html', content)
 
 def blogs_with_type(request, blog_type_pk):
     content = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     content['blogs'] = Blog.objects.filter(blog_type=blog_type)
     content['blog_type'] = blog_type
-    return render(request, 'blogs_with_type.html', content)
+    return render(request, 'blog/blogs_with_type.html', content)
