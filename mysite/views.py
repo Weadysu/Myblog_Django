29717f15c95c5da186846499a0df9a59573c8c4b
SuHuadi <19403372@life.hkbu.edu.hk>
2020-02-04 01:02:39 +0800

    NO.20 of Django tutorial on Bilibili(Reading count statistics and display)

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import get_reading_data_within_seven_days
+from blog.models import Blog
 
 def home(request):
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_reading_data_within_seven_days(blog_content_type)
+
     content = {}
+    content['dates'] = dates
+    content['read_nums'] = read_nums
     return render(request, 'home.html', content)
--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -1,16 +1,33 @@
+import datetime
 from django.contrib.contenttypes.models import ContentType
-from .models import ReadNum
+from django.utils import timezone
+from django.db.models import Sum
+from .models import ReadNum, ReadDetail
 
 def read_statistics_once_read(request, obj):
     ct = ContentType.objects.get_for_model(obj)
     key = "%s_%s_readed" % (ct.model, obj.pk)
     if not request.COOKIES.get(key):
-            if ReadNum.objects.filter(content_type=ct, object_id=obj.pk).count():
-                # Corresponding record exits
-                readnum = ReadNum.objects.get(content_type=ct, object_id=obj.pk)
-            else:
-                # Corresponding record does not exit
-                readnum = ReadNum(content_type=ct, object_id=obj.pk)
-            readnum.read_num += 1
-            readnum.save()
+        # Number of total readed blogs pluses 1
+        readnum, created = ReadNum.objects.get_or_create(content_type=ct, object_id=obj.pk)
+        readnum.read_num += 1
+        readnum.save()
+
+        # Number of readed blogs of the day pluses 1
+        date = timezone.now().date()
+        deadDetail, created = ReadDetail.objects.get_or_create(content_type=ct, object_id=obj.pk, date=date)
+        deadDetail.read_num += 1
+        deadDetail.save()
     return key
+
+def get_reading_data_within_seven_days(content_type):
+    today = timezone.now().date()
+    read_nums = []
+    dates = []
+    for i in range(7, 0, -1):
+        date = today  - datetime.timedelta(days=i)
+        dates.append(date.strftime('%m/%d'))
+        read_details = ReadDetail.objects.filter(content_type=content_type, date=date)
+        result = read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return dates, read_nums
